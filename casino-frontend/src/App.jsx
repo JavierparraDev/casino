e1d653f312f870922e19f4bde53f8fdbefafa388
JavierparraDev <javier00parra@gmail.com>
2024-11-29 00:27:44 -0500

    fix-Header

--- casino-frontend/src/App.jsx
+++ casino-frontend/src/App.jsx
@@ -9,12 +9,15 @@ import BetsHistory from './pages/BetsHistory';
 import AdminBalance from './pages/AdminBalance';
 import RecargaPage from './pages/RecargaPage';
 import RetiroPage from './pages/RetiroPage';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route element={<Layout />}>
+
         <Route path="/" element={<DashboardPage />} />
         <Route path="/bet" element={<BetPage />} />
         <Route path="/register" element={<RegisterPage />} />
@@ -23,6 +26,7 @@ function App() {
         <Route path="/adminBalance" element={<AdminBalance />} />
         <Route path="/recargaPage" element={<RecargaPage />} />
         <Route path="/retiroPage" element={<RetiroPage />} />
+        </Route>
       </Routes>
     </Router>
   );
